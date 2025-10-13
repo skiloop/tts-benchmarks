99e428e55c8cd6f620f42bdfdf75e79a232a9f2c
skiloop <skiloop@gmail.com>
2025-10-13 17:59:07 +0800

    chore: refactor model preparation script to use environment variables for Python executable paths

--- prepare-models.sh
+++ prepare-models.sh
@@ -6,27 +6,24 @@ TEST_TEXT="My name is Dave, and um, I'm from London"
 REF_AUDIO=./samples/dave.wav
 REF_TEXT="./samples/dave.txt"
 REF_CODES="samples/dave.pt"
-
+envs_path=${CONDA_PATH:-/usr/local/envs/}
 # Test with different configurations
-conda activate neutts-air
-python -m examples.onnx_example \
+envs_path/neutts-air/bin/python -m examples.onnx_example \
   --input_text $TEST_TEXT \
   --ref_codes $REF_CODES \
   --ref_text $REF_TEXT \
   --backbone neuphonic/neutts-air-q4-gguf
-python -m examples.basic_example \
+envs_path/neutts-air/bin/python -m examples.basic_example \
   --input_text $TEST_TEXT \
   --ref_audio $REF_AUDIO \
   --ref_text $REF_TEXT \
   --backbone neuphonic/neutts-air-q4-gguf
-python -m examples.basic_example \
+envs_path/neutts-air/bin/python -m examples.basic_example \
   --input_text $TEST_TEXT \
   --ref_audio $REF_AUDIO \
   --ref_text $REF_TEXT
-python -m examples.onnx_example --input_text $TEST_TEXT --ref_audio $REF_AUDIO --ref_text $REF_TEXT --ref_codes $REF_CODES
-conda activate chartts
-chartts -t $TEST_TEXT -o output.wav
+envs_path/neutts-air/bin/python -m examples.onnx_example --input_text $TEST_TEXT --ref_audio $REF_AUDIO --ref_text $REF_TEXT --ref_codes $REF_CODES
+envs_path/chartts/bin/chartts -t $TEST_TEXT -o output.wav
 # coqui tts
-conda activate coqui-tts
-tts --text "$TEST_TEXT" --out_path output.wav
-tts --test "$TEST_TEXT" --out_path output.wav --speaker_wav $REF_AUDIO --model_name tts_models/multilingual/multi-dataset
+envs_path/coqui-tts/bin/tts --text "$TEST_TEXT" --out_path output.wav
+envs_path/coqui-tts/bin/tts --test "$TEST_TEXT" --out_path output.wav --speaker_wav $REF_AUDIO --model_name tts_models/multilingual/multi-dataset
