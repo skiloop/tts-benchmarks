913c74616310216aa310d6041a28d471df228a54
skiloop <skiloop@gmail.com>
2025-10-13 17:36:21 +0800

    chore: update environment setup script to streamline conda environment creation and installation of dependencies

--- create-env.sh
+++ create-env.sh
@@ -2,18 +2,14 @@
 
 # create different environments for different models with conda
 neutts-air_path=${NEUTTS_AIR_PATH:-/content/neutts-air}
+apt-get install -y espeak time
 
-conda create -n chartts python=3.12
-conda activate chartts
-pip install ChatTTS
+conda init
+conda create -y -n chartts python=3.12 ChatTTS
 
-conda create -n coqui-tts python=3.10
-conda activate coqui-tts
-pip install TTS
+conda create -y -n coqui-tts python=3.10 TTS
 
 # env for neutts-air and edge-tts
-conda create -n neutts-air python=3.12
+conda create -y -n neutts-air python=3.12 edge-tts fish-speech ffmpeg onnxruntime llama-cpp-python
 conda activate neutts-air
-apt-get install -y espeak time ffmpeg
-pip install $neutts-air/requirements.txt
-pip install llama-cpp-python onnxruntime edge-tts
+pip install $neutts-air_path/requirements.txt
