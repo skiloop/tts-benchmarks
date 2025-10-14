f7cf08322e3d2c9a3c7eff3b00ffbfba5e79476d
skiloop <skiloop@gmail.com>
2025-10-14 09:44:06 +0800

    chore: update create-env.sh to include edge-tts installation in the neutts-air environment setup

--- create-env.sh
+++ create-env.sh
@@ -7,9 +7,10 @@ neutts-air_path=${NEUTTS_AIR_PATH:-/content/neutts-air}
 apt-get install -y espeak time
 
 conda create -y -n chartts python=3.12 
-$envs_path/chartts/bin/pip install
+$envs_path/chartts/bin/pip install ChatTTS
 conda create -y -n coqui-tts python=3.10 
 $envs_path/coqui-tts/bin/pip install TTS
 # env for neutts-air and edge-tts
-conda create -y -n neutts-air python=3.12 edge-tts fish-speech ffmpeg onnxruntime llama-cpp-python
+conda create -y -n neutts-air python=3.12 fish-speech ffmpeg onnxruntime llama-cpp-python
 $envs_path/neutts-air/bin/pip install -r ${neutts-air_path}/requirements.txt
+$envs_path/neutts-air/bin/pip install edge-tts
