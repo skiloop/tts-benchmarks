2573dd00bb24c560c30d05d5bc5c3de46896181c
skiloop <skiloop@gmail.com>
2025-10-14 09:31:25 +0800

    chore: enhance environment setup script to include pip installation commands for conda environments and improve path handling

--- create-env.sh
+++ create-env.sh
@@ -6,9 +6,10 @@ envs_path=${CONDA_PATH:-/usr/local/envs/}
 neutts-air_path=${NEUTTS_AIR_PATH:-/content/neutts-air}
 apt-get install -y espeak time
 
-conda create -y -n chartts python=3.12 ChatTTS
-conda create -y -n coqui-tts python=3.10 TTS
-
+conda create -y -n chartts python=3.12 
+$envs_path/chartts/bin/pip install
+conda create -y -n coqui-tts python=3.10 
+$envs_path/coqui-tts/bin/pip install TTS
 # env for neutts-air and edge-tts
 conda create -y -n neutts-air python=3.12 edge-tts fish-speech ffmpeg onnxruntime llama-cpp-python
-$envs_path/neutts-air/bin/pip install $neutts-air_path/requirements.txt
+$envs_path/neutts-air/bin/pip install -r ${neutts-air_path}/requirements.txt
