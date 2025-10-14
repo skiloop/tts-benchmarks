a73b8bc7676846ef924f2fb603fd4bfa4d3931df
skiloop <skiloop@gmail.com>
2025-10-14 09:55:26 +0800

    chore: fix variable naming in create-env.sh for consistency in path handling

--- create-env.sh
+++ create-env.sh
@@ -3,7 +3,7 @@
 # create different environments for different models with conda
 . /usr/local/etc/profile.d/conda.sh
 envs_path=${CONDA_PATH:-/usr/local/envs/}
-neutts-air_path=${NEUTTS_AIR_PATH:-/content/neutts-air}
+neutts_air_path=${NEUTTS_AIR_PATH:-/content/neutts-air}
 apt-get install -y espeak time
 
 conda create -y -n chartts python=3.12 
@@ -12,5 +12,5 @@ conda create -y -n coqui-tts python=3.10
 $envs_path/coqui-tts/bin/pip install TTS
 # env for neutts-air and edge-tts
 conda create -y -n neutts-air python=3.12 fish-speech ffmpeg onnxruntime llama-cpp-python
-$envs_path/neutts-air/bin/pip install -r ${neutts-air_path}/requirements.txt
+$envs_path/neutts-air/bin/pip install -r ${neutts_air_path}/requirements.txt
 $envs_path/neutts-air/bin/pip install edge-tts
