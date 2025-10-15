cd324517978295c970a2ec497b0fa4fcb55857e4
skiloop <skiloop@gmail.com>
2025-10-15 09:23:32 +0800

    chore: update create-env.sh to specify Python versions and include transformers installation for ChatTTS and TTS

--- create-env.sh
+++ create-env.sh
@@ -7,9 +7,9 @@ neutts_air_path=${NEUTTS_AIR_PATH:-/content/neutts-air}
 apt-get install -y espeak time
 
 conda create -y -n chartts python=3.12 
-$envs_path/chartts/bin/pip install ChatTTS
-conda create -y -n coqui-tts python=3.10 
-$envs_path/coqui-tts/bin/pip install TTS
+$envs_path/chartts/bin/pip install ChatTTS transformers==4.53.2
+conda create -y -n coqui-tts python=3.9
+$envs_path/coqui-tts/bin/pip install TTS transformers==4.53.2
 # env for neutts-air and edge-tts
 conda create -y -n neutts-air python=3.12 ffmpeg onnxruntime llama-cpp-python pyaudio
 $envs_path/neutts-air/bin/pip install -r ${neutts_air_path}/requirements.txt
